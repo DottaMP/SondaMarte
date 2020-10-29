16e9ac4129468b8a51b4830f30b2a4ca519b8641
DottaMP <dotta.may@gmail.com>
2020-10-29 17:14:58 -0300

    procedural

--- README.md
+++ README.md
@@ -1,2 +1,83 @@
-# SondaMarte
-Explorando_marte_programming_test
+# Explorando Marte :rocket:
+
+**Teste de Programação Elo7** [Repositório Teste](https://gist.github.com/elo7-developer/1a40c96a5d062b69f02c "Clique Aqui").
+
+## Objetivo
+Criar um programa que processe uma série de instruções enviadas para as sondas que estão explorando Marte.
+O planalto é divido numa malha para simplificar a navegação. 
+Para controlar as sondas, a NASA envia uma simples sequência de letras. As letras possíveis são "L", "R" e "M". Destas, "L" e "R" fazem a sonda virar 90 graus para a esquerda ou direita, respectivamente, sem mover a sonda. "M" faz com que a sonda mova-se para a frente um ponto da malha, mantendo a mesma direção.
+Forma de *entrada* e *saída* é livre.
+
+### Pensando na Solução
+Foi realizado primeiro um programa simples de forma *procedural*,[Link no código](https://gist.github.com/elo7-developer/1a40c96a5d062b69f02c "Clique Aqui"), pensando como prioridade a lógica que seria utilizada para que a saída ocorresse conforme o esperado.    
+Após chegar na lógica esperada, foi pensando na aplicação feita através de métodos utilizando a *orientação a objetos*,[Link no código](https://gist.github.com/elo7-developer/1a40c96a5d062b69f02c "Clique Aqui"), onde foi construído o main 'sondaMarte' responsável por chamar os métodos da classe instrucoesSonda.
+
+<br>
+
+### Entrada
+*Incluído a Letra 'S' para parar os comandos.*
+A entrada de dados foi construída para que fosse solicitado ao usuário as informações, tamanho do terreno, localização inicial da sonda e comandos realizados por essa. Porém é possível também fixar valores nos métodos, fazendo com que não seja necessário que o usuário informe dados.    
+
+| Entrada dos Dados | 
+|------|
+|Informe o tamanho da área do terreno:|
+|Informe o tamanho de x: 5|
+|Informe o tamanho de y: 5|
+||
+|Informe a posição da Sonda:|
+|Entre com a posição de x: 1|
+|Entre com a posição de y: 2|
+|Entre com a direção cardial: N|
+||
+|Posição Inicial: 1, 2, N|
+|Escolha os comandos para a sonda realizar:| 
+|[L] - Para girar para esquerda: |
+|[R] - Para girar para direita: |
+|[M] - Para mover para frente mantendo a mesma direção: |
+|[S] - Parar: |
+|Digite o comando desejado: |
+|L|
+|M|
+|L|
+|M|
+|L|
+|M|
+|L|
+|M|
+|M|
+|S|
+|Informe o tamanho da área do terreno:|
+|Informe o tamanho de x: 5|
+|Informe o tamanho de y: 5|
+||
+|Informe a posição da Sonda:|
+|Entre com a posição de x: 3|
+|Entre com a posição de y: 3|
+|Entre com a direção cardial: E|
+||
+|Posição Inicial: 3, 3, E|
+|Escolha os comandos para a sonda realizar:| 
+|[L] - Para girar para esquerda: |
+|[R] - Para girar para direita: |
+|[M] - Para mover para frente mantendo a mesma direção: |
+|[S] - Parar: |
+|Digite o comando desejado: |
+|M|
+|M|
+|R|
+|M|
+|M|
+|M|
+|Fora da área permitida!|
+|R|
+|R|
+|M|
+|Fora da área permitida!|
+|S| 
+ 
+## Saída
+
+| Saída dos Dados | 
+|------|
+|Posição Final 1ª Sonda: 1, 3, N|
+|Posição Final 2ª Sonda: 5, 1, E|
---
+++ procedural/proceduralMarteSonda.java
@@ -0,0 +1,215 @@
+package testeDeProgramaçãoElo7;
+
+import java.util.*;
+public class proceduralMarteSonda {
+	
+	private static Scanner ler;
+
+	public static void main(String[] args)
+	{
+		ler = new Scanner(System.in);
+		
+		//Declaração das variáveis
+		int a, l, x1 , y1, x2, y2;
+		String opcao, direcao1, direcao2;
+		
+		
+		System.out.print("Informe o tamanho do terreno - Altura: ");
+		a = ler.nextInt();
+		//Solicitado para que o usuário informe o tamanho do terreno.
+		System.out.print("Informe o tamanho do terreno - Largura: ");
+		l = ler.nextInt();
+		
+		//1ª Sonda
+		System.out.println("Informe a localização da 1ª Sonda: ");
+		//Solicitado para que o usuário informe s localicação inicial da 1ª Sonda, devendo informar as posições de x, y e direção cardial.
+		System.out.print("Informe a localização de x: ");
+		x1 = ler.nextInt();
+		System.out.print("Informe a localização de y: ");
+		y1 = ler.nextInt();
+		System.out.print("Informe a posição N, S, E ou W: ");
+		direcao1 = ler.next().toUpperCase(); //O método '.toUpperCase()'converte toda a String para caixa alta.
+
+		System.out.println("Escolha os comandos para a sonda realizar: ");
+		//Solicita ao usuário para informar os movimentos que a sonda irá realizar,
+		//As entradas aceitas serão: 'L' para girar a sonda para esquerda, 'R' para girar para direita, 'M' para andar parfa frente e 'S' para sair.
+		System.out.println("[L] - Para girar para esquerda: ");
+		System.out.println("[R] - Para girar para direita: "); 
+		System.out.println("[M] - Para mover para frente mantendo a mesma direção: ");
+		System.out.println("[S] - Parar: ");
+		System.out.println("Digite o comando desejado: ");
+		
+		do{ //O usuário irá informar os movimentos enquanto a entrada for != 'S' ou outro dado diferente do solicitado.
+			
+			opcao = ler.next().toUpperCase(); //O método '.toUpperCase()'converte toda a String para caixa alta.
+			
+			switch (opcao){
+			
+			case "L"://Caso a entrada for igual a 'L' será realizado a lógica abaixo:
+				if (direcao1.equals("N")){
+				//Se a direção cardial atual for igual a N, será alterado para a direção W.
+					direcao1 = "W";
+					
+				}else if (direcao1.equals("E")){
+				//Senão se a direção cardial atual for igual a E, será alterado para a direção N.
+					direcao1 = "N";
+					
+				}else if (direcao1.equals("S")){
+				//Senão se a direção cardial atual for igual a S, será alterado para a direção E.
+					direcao1 = "E";
+					
+				}else if (direcao1.equals("W")){
+				//Senão se a direção cardial atual for igual a W, será alterado para a direção S.
+					direcao1 = "S";
+				}
+				break;
+			
+			case "R": //Caso a entrada for igual a 'R' será realizado a lógica abaixo:
+				if (direcao1.equals("N")){
+				//Se a direção cardial atual for igual a N, será alterado para a direção E.
+					direcao1 = "E";
+					
+				}else if (direcao1.equals("E")){
+				//Senão se a direção cardial atual for igual a E, será alterado para a direção S.
+					direcao1 = "S";
+					
+				}else if (direcao1.equals("S")){
+				//Senão se a direção cardial atual for igual a S, será alterado para a direção W.
+					direcao1 = "W";
+				}else if (direcao1.equals("W")){
+				//Senão se a direção cardial atual for igual a W, será alterado para a direção N.
+					direcao1 = "N";
+				}
+				break;
+			
+			case "M": //Caso a entrada for igual a 'M' será realizado a lógica abaixo:
+				// Caso as posições x ou y forem menor do que l ou a, não será possível realizar o movimento, pois estará fora da área permitida.
+				if (direcao1.equals("N") && y1 < l){
+				//Se a direção cardial atual for igual a N e a posição de y for menor do que l, y irá andar para frente.
+					y1 = y1 + 1;
+					
+				}else if (direcao1.equals("S") && y1 < l){
+				//Senão se a direção cardial atual for igual a S e a posição de y for menor do que l, y irá andar para trás.
+					y1 = y1 - 1;
+					
+				}else if (direcao1.equals("E") && x1 < a){
+				//Senão se a direção cardial atual for igual a E e a posição de x for menor do que a, x irá andar para frente.
+					x1 = x1 + 1;
+				}else if (direcao1.equals("W") && x1 < a){
+				//Senão se a direção cardial atual for igual a W e a posição de x for menor do que a, x irá andar para trás.
+					x1 = x1 - 1;
+					
+				}else {
+				//Senão o movimento não será computado, pois entende-se que estará fora do terreno.
+					System.out.println("Fora da área permitida!"); //Movimento não computado.
+				}
+				break;
+			
+			case "S": //Caso a entrada for igual a 'S' será interrompido os movimentos.
+				System.out.println("\n\n");
+				break;
+				
+			default: //Caso a entrada seja diferente das permitidas irá aparecer a mensagem abaixo.
+				System.out.println("Você digitou uma opção inválida!");
+			}		
+		}while(opcao.equals("L") || opcao.equals("M") || opcao.equals("R"));
+		
+		//2ª Sonda
+		System.out.println("Informe a localização da 2ª Sonda: ");
+		//Solicitado para que o usuário informe s localicação inicial da 2ª Sonda, devendo informar as posições de x, y e direção cardial.
+		System.out.print("Informe a localização de x: ");
+		x2 = ler.nextInt();
+		System.out.print("Informe a localização de y: ");
+		y2 = ler.nextInt();
+		System.out.print("Informe a posição N, S, E ou W: ");
+		direcao2 = ler.next().toUpperCase(); //O método '.toUpperCase()'converte toda a String para caixa alta.
+
+		System.out.println("Escolha os comandos para a sonda realizar: ");
+		//Solicita ao usuário para informar os movimentos que a sonda irá realizar,
+		//As entradas aceitas serão: 'L' para girar a sonda para esquerda, 'R' para girar para direita, 'M' para andar parfa frente e 'S' para sair.
+		System.out.println("[L] - Para virar para esquerda: ");
+		System.out.println("[R] - Para virar para direita: "); 
+		System.out.println("[M] - Para mover para frente mantendo a mesma direção: ");
+		System.out.println("[S] - Parar: ");
+		System.out.println("Digite o comando desejado: ");
+		
+		do{ //O usuário irá informar os movimentos enquanto a entrada for != 'S' ou outro dado diferente do solicitado.
+			
+			opcao = ler.next().toUpperCase(); //O método '.toUpperCase()'converte toda a String para caixa alta.
+			
+			switch (opcao){
+			
+			case "L"://Caso a entrada for igual a 'L' será realizado a lógica abaixo:
+				if (direcao1.equals("N")){
+				//Se a direção cardial atual for igual a N, será alterado para a direção W.
+					direcao1 = "W";
+					
+				}else if (direcao1.equals("E")){
+				//Senão se a direção cardial atual for igual a E, será alterado para a direção N.
+					direcao1 = "N";
+					
+				}else if (direcao1.equals("S")){
+				//Senão se a direção cardial atual for igual a S, será alterado para a direção E.
+					direcao1 = "E";
+					
+				}else if (direcao1.equals("W")){
+				//Senão se a direção cardial atual for igual a W, será alterado para a direção S.
+					direcao1 = "S";
+				}
+				break;
+			
+			case "R": //Caso a entrada for igual a 'R' será realizado a lógica abaixo:
+				if (direcao1.equals("N")){
+				//Se a direção cardial atual for igual a N, será alterado para a direção E.
+					direcao1 = "E";
+					
+				}else if (direcao1.equals("E")){
+				//Senão se a direção cardial atual for igual a E, será alterado para a direção S.
+					direcao1 = "S";
+					
+				}else if (direcao1.equals("S")){
+				//Senão se a direção cardial atual for igual a S, será alterado para a direção W.
+					direcao1 = "W";
+				}else if (direcao1.equals("W")){
+				//Senão se a direção cardial atual for igual a W, será alterado para a direção N.
+					direcao1 = "N";
+				}
+				break;
+			
+			case "M": //Caso a entrada for igual a 'M' será realizado a lógica abaixo:
+				// Caso as posições x ou y forem menor do que l ou a, não será possível realizar o movimento, pois estará fora da área permitida.
+				if (direcao1.equals("N") && y1 < l){
+				//Se a direção cardial atual for igual a N e a posição de y for menor do que l, y irá andar para frente.
+					y1 = y1 + 1;
+					
+				}else if (direcao1.equals("S") && y1 < l){
+				//Senão se a direção cardial atual for igual a S e a posição de y for menor do que l, y irá andar para trás.
+					y1 = y1 - 1;
+					
+				}else if (direcao1.equals("E") && x1 < a){
+				//Senão se a direção cardial atual for igual a E e a posição de x for menor do que a, x irá andar para frente.
+					x1 = x1 + 1;
+				}else if (direcao1.equals("W") && x1 < a){
+				//Senão se a direção cardial atual for igual a W e a posição de x for menor do que a, x irá andar para trás.
+					x1 = x1 - 1;
+					
+				}else {
+				//Senão o movimento não será computado, pois entende-se que estará fora do terreno.
+					System.out.println("Fora da área permitida!"); //Movimento não computado.
+				}
+				break;
+			
+			case "S": //Caso a entrada for igual a 'S' será interrompido os movimentos.
+				System.out.println("\n\n");
+				break;
+				
+			default: //Caso a entrada seja diferente das permitidas irá aparecer a mensagem abaixo.
+				System.out.println("Você digitou uma opção inválida!");
+			}		
+		}while(opcao.equals("L") || opcao.equals("M") || opcao.equals("R"));
+		
+		// Será apresentado a posição final das duas sondas, pegando a informação de 'x', 'y', 'direcao'.
+		System.out.printf("\nPosição final:\n	1ª Sonda: %d %d %s\n	2ª Sonda: %d %d %s", x1, y1, direcao1, x2, y2, direcao2);
+
+	}
+}
